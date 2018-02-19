508fb58a8fcac7bb7c224aab24072a5bc1b212c8
kaspiotr <kaspiotr@gmail.com>
2018-02-19 15:15:59 +0100

    Add insertion sort algorithm using an array of elements

--- Useful_algorithms_and_code_snippets/Array_insertion_sort.cpp
+++ Useful_algorithms_and_code_snippets/Array_insertion_sort.cpp
@@ -1,4 +1,42 @@
-//
-// Created by kaspiotr on 2/17/18.
-//
+//author: @kaspiotr (Piotr Kasprzyk)
+
+//Implementation of insertion sort algorithm on array of elements
+
+#include <iostream>
+
+using namespace std;
+
+void printArray(int A[], int n) {
+    for(int i=0; i<n; i++) {
+        cout<<A[i]<<"\t";
+    }
+    cout<<endl;
+}
+
+void insertionSort(int A[], int n) {
+    int j, tmp;
+    for(int i=0; i<n; i++) {
+        tmp = A[i];
+        j = i-1;
+        while(j >=0 && A[j] >tmp) {
+            A[j+1]=A[j];
+            j--;
+        }
+        A[j+1]=tmp;
+    }
+}
+
+int main() {
+
+    int A[]={5,2,4,6,1,3};
+    int n=6;
+
+    printArray(A, n);
+
+    insertionSort(A, n);
+
+    printArray(A, n);
+
+    return 0;
+}
 
